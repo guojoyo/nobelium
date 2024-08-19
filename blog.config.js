d372aacde13c08f0605de35d6e50b3fa27b56636
GuoJoyo <guojoyo@qq.com>
2024-08-19 08:50:58 +0800

    Update blog.config.js
    edit url

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: '突然心血来潮',
   author: 'Guojoyo',
   email: 'guojoyo@live.com',
-  link: 'https://guojoyo.vercel.app',
+  link: 'https://joyo.us.kg',
   description: '突然心血来潮，记录生活.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/guojoyo',
+  socialLink: 'https://joyo.us.kg',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
